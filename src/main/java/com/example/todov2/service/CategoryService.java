5722053fadd59b3d0bd7615a9bbdffb94ea4b855
MishailEx <mishail.avram@mail.ru>
2023-10-24 19:10:23 +0300

    gggg

--- src/main/java/com/example/todov2/service/CategoryService.java
+++ src/main/java/com/example/todov2/service/CategoryService.java
@@ -39,6 +39,7 @@ public class CategoryService {
     public List<Category> search(CategorySearchValues searchValues) {
         System.out.println("aaa");
         System.out.println("aaa");
+        System.out.println("aaa");
         return categoryRepository.findByTitleAll(searchValues.getText());
     }
 }
